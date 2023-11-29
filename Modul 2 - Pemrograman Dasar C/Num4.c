ad9abbafb020b3f603626ec8a28abba8266cd744
Fattan Naufan Islami <147119223+Rainy1501@users.noreply.github.com>
2023-11-29 20:56:15 +0700

    Add files via upload

---
+++ Modul 2 - Pemrograman Dasar C/Num4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	//Membuat variabel
+	int panjang = 10; 
+	int lebar = 5; // Untuk memperkenalkan komputer dengan variable Nama
+	int luas;	
+
+	luas = 2 * panjang + 2 * lebar;
+	
+	printf("\n------------------------------------\n");
+	printf("Jadi luas persegi panjang ialah : %i", luas); // Digunakan untuk membuat garis baru
+
+	return 0;
+}
