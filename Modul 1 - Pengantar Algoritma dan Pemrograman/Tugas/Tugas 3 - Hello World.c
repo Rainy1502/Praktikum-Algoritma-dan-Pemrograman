65f3a85df001f7197c8854e6d3640a0bdb885322
Fattan Naufan Islami <153074952+Rainy1502@users.noreply.github.com>
2023-12-15 17:38:44 +0700

    Add files via upload

---
+++ Modul 1 - Pengantar Algoritma dan Pemrograman/Tugas/Tugas 3 - Hello World.c
@@ -0,0 +1,26 @@
+/*
+Nama  : Fattan Naufan Islami
+NIM   : 23343037
+Prodi : Informatika
+*/
+
+#include <stdio.h> 
+// Ialah sebuah pustaka dari sebuah kode yang bakal digunakan seperti contohnya  printf, scanf 
+
+/* sebuah eksekusi fungsi utama yang berfungsi sebagai starting point dari program c */
+int main(){ 
+	
+	// Sebuah judul dari program
+    printf("      ___            __           __   __        __ \n"); 
+    printf("|__| |__  |    |    /  \\    |  | /  \\ |__) |    |  \ \n"); 
+    printf("|  | |___ |___ |___ \\__/    |/\\| \\__/ |  \\ |___ |__/ \n"); 
+    printf("-----------------------------------------------------\n");
+    printf("\n");
+
+	printf("Hello World");  /* Perintah yang digunakan untuk mencetak teks Hello World ke output layar kita. 
+                              Teks harus diberikan tanda kutip agar tidak terjadi kesalahan di karenakan kompilator
+                              tidak akan menemukan teks yang kita tulis oleh karena variabel tersebut tidak 
+                                                         di deklarasikan*/
+	return 0;/* Sebuah perintah yang digunakan untuk menunjukkan bahwa program telah selesai dan akan
+                              mengembalikan nilai 0 kepada sistem operasi  */
+}
